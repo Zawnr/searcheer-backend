855f3fbb53caea39df7768cb19eb9c0e7a04622d
zawawi <rochmat27nurza@gmail.com>
2025-06-08 12:37:45 +0700

    chore: add deploy script for production

--- src/server.js
+++ src/server.js
@@ -9,12 +9,13 @@ const Jwt = require('@hapi/jwt');
 const init = async () => {
   const server = Hapi.server({
     port: process.env.PORT || 3000,
-    host: process.env.HOST || 'localhost',
+    host: process.env.HOST || '0.0.0.0',
     routes: {
       cors: {
         // Ganti '*' dengan domain frontend untuk produksi
-        origin: ['*'],
-        // origin: [process.env.FRONTEND_URL], //nanti diganti ke ini kalo sudah deploy
+        // origin: ['*'],
+        //nanti diganti ke ini kalo sudah deploy
+        origin: [process.env.FRONTEND_URL],
       },
     },
   });
